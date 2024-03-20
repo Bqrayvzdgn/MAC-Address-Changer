e12a3bf763c4c8581163fbb678e5f1c9129f407c
Buğrahan Y <78935299+Bqrayvzdgn@users.noreply.github.com>
2024-03-20 17:52:09 +0300

    Update macchange.py

--- macchange.py
+++ macchange.py
@@ -13,8 +13,8 @@ Logo = """
 
 def get_user_input():
     parser = argparse.ArgumentParser(Logo, description="This application was developed by kenxzz.", usage="python3 macchanger.py -i [interface] -m [XX:XX:XX:XX:XX:XX]", epilog="You can access the source codes at https://github.com/Bqrayvzdgn/Macchanger.")
-    parser.add_argument("-i", "--iface", dest="interface", help="Enter your network interface")
-    parser.add_argument("-m", "--mac", dest="mac_address", help="Enter your mac address")
+    parser.add_argument("-i", "--interface", dest="interface", help="Enter your network interface")
+    parser.add_argument("-m", "--mac_address", dest="mac_address", help="Enter your mac address")
     return parser.parse_args()
 
 def change_mac_address(user_interface, user_mac_address):
@@ -31,12 +31,12 @@ def control_new_mac(interface):
         return None
 
 if __name__ == "__main__":
+    print(Logo)
     (user_input, args) = get_user_input()
     change_mac_address(user_input.interface,user_input.mac_address)
     finalized_mac = control_new_mac(str(user_input.interface))
 
     if finalized_mac == user_input.mac_address:
-        print(Logo)
         print("MAC Address is created!")
     else:
         print("An error occurred while creating the MAC Address!")
