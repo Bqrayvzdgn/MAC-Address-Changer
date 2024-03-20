a5493057bbedcda5edaea07379b3087aa8c3ac8f
Buğrahan Y <78935299+Bqrayvzdgn@users.noreply.github.com>
2024-03-20 16:36:36 +0300

    Update macchanger.py

--- macchanger.py
+++ macchanger.py
@@ -17,22 +17,22 @@ def get_user_input():
     parse_object = optparse.OptionParser(description="This application was developed by kenxzz.", usage="python macchanger.py -h", epilog="[ Default ] python3 macchanger.py -i [interface] -m [XX:XX:XX:XX:XX:XX]")
     parse_object.add_option("-i", "--iface", dest="interface", help="Interface to change!")
     parse_object.add_option("-m", "--mac", dest="mac_address", help="New mac address")
-    parse_object.add_option("-s", "--show", help="Network interface show", action="store_true")
-    parse_object.add_option("-R", "--random", dest="random", help="Random mac address", action="store_true")
-    parse_object.add_option('-r', '--reset', help="Reset to Original MAC", action="store_true")
+    # parse_object.add_option("-s", "--show", help="Network interface show", action="store_true")
+    # parse_object.add_option("-R", "--random", dest="random", help="Random mac address", action="store_true")
+    # parse_object.add_option('-r', '--reset', help="Reset to Original MAC", action="store_true")
     return parse_object.parse_args()
 
-def get_random_mac_address():
-    uppercased_hexdigits = ''.join(set(string.hexdigits.upper()))
-    mac = ""
-    for i in range(6):
-        for j in range(2):
-            if i == 0:
-                mac += random.choice("02468ACE")
-            else:
-                mac += random.choice(uppercased_hexdigits)
-        mac += ":"
-    return mac.strip(":")
+# def get_random_mac_address():
+#     uppercased_hexdigits = ''.join(set(string.hexdigits.upper()))
+#     mac = ""
+#     for i in range(6):
+#         for j in range(2):
+#             if i == 0:
+#                 mac += random.choice("02468ACE")
+#             else:
+#                 mac += random.choice(uppercased_hexdigits)
+#         mac += ":"
+#     return mac.strip(":")
 
 def change_mac_address(interface, mac_address):
     subprocess.call(["ifconfig", interface, "down"])
