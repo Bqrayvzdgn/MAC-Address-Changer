72c57de27a93f9c5f9d46517d11ff0bf5eeeb55f
Buğrahan Y <78935299+Bqrayvzdgn@users.noreply.github.com>
2024-03-20 17:30:35 +0300

    Update macchange.py

--- macchange.py
+++ macchange.py
@@ -15,7 +15,6 @@ def get_user_input():
     parser = argparse.ArgumentParser(description="This application was developed by kenxzz.", usage="python macchanger.py -h", epilog="[ Default ] python3 macchanger.py -i [interface] -m [XX:XX:XX:XX:XX:XX]")
     parser.add_argument("-i", "--iface", dest="interface", help="Interface to change!")
     parser.add_argument("-m", "--mac", dest="mac_address", help="New mac address")
-    parser.add_argument("-r", "--random", action="store_true", help="Generate a random MAC address")
     return parser.parse_args()
 
 def change_mac_address(user_interface, user_mac_address):
@@ -24,8 +23,8 @@ def change_mac_address(user_interface, user_mac_address):
     subprocess.call(["ifconfig", user_interface, "up"])
 
 def control_new_mac(interface):
-    ifconfig = subprocess.check_output(["ifconfig", interface])
-    new_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig))
+    output = subprocess.check_output(["ifconfig", interface])
+    new_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(output))
     if new_mac:
         return new_mac.group(0)
     else:
