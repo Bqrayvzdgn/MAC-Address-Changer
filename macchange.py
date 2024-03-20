26870affad2ef68cceaded9c7516456085d1d842
Buğrahan Y <78935299+Bqrayvzdgn@users.noreply.github.com>
2024-03-20 17:06:36 +0300

    Update macchange.py

--- macchange.py
+++ macchange.py
@@ -1,7 +1,7 @@
 import random
 import string
 import subprocess
-import optparse
+import argparse
 import re
 
 Logo = """
@@ -14,12 +14,10 @@ Logo = """
 """
 
 def get_user_input():
-    parse_object = optparse.OptionParser(description="This application was developed by kenxzz.", usage="python macchanger.py -h", epilog="[ Default ] python3 macchanger.py -i [interface] -m [XX:XX:XX:XX:XX:XX]")
-    parse_object.add_option("-i", "--iface", dest="interface", help="Interface to change!")
-    parse_object.add_option("-m", "--mac", dest="mac_address", help="New mac address")
-    parse_object.add_option("-r", "--random", dest="random", help="Random mac address")
-
-    return parse_object.parse_args()
+    parser = argparse.ArgumentParser(description="This application was developed by kenxzz.", usage="python macchanger.py -h", epilog="[ Default ] python3 macchanger.py -i [interface] -m [XX:XX:XX:XX:XX:XX]")
+    parser.add_argument("-i", "--iface", dest="interface", help="Interface to change!")
+    parser.add_argument("-m", "--mac", dest="mac_address", help="New mac address")
+    return parser.parse_args()
 
 def get_random_mac_address():
     uppercased_hexdigits = ''.join(set(string.hexdigits.upper()))
@@ -33,14 +31,14 @@ def get_random_mac_address():
         mac += ":"
     return mac.strip(":")
 
-def change_mac_address(user_interface,user_mac_address):
-    subprocess.call(["ifconfig",user_interface,"down"])
-    subprocess.call(["ifconfig", user_interface,"hw","ether",user_mac_address])
-    subprocess.call(["ifconfig",user_interface,"up"])
+def change_mac_address(user_interface, user_mac_address):
+    subprocess.call(["ifconfig", user_interface, "down"])
+    subprocess.call(["ifconfig", user_interface, "hw", "ether", user_mac_address])
+    subprocess.call(["ifconfig", user_interface, "up"])
 
 def control_new_mac(interface):
-    ifconfig = subprocess.check_output(["ifconfig",interface])
-    new_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",str(ifconfig))
+    ifconfig = subprocess.check_output(["ifconfig", interface])
+    new_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig))
     if new_mac:
         return new_mac.group(0)
     else:
@@ -48,10 +46,12 @@ def control_new_mac(interface):
 
 if __name__ == "__main__":
     print(Logo)
-    (user_input, arguments) = get_user_input()   
+    user_input = get_user_input()
+    
     if user_input.mac_address:
         change_mac_address(user_input.interface, user_input.mac_address)
         finalized_mac = control_new_mac(str(user_input.interface))
+
         if finalized_mac == user_input.mac_address:
             print("MAC Address is successfully changed!")
         else:
