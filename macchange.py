5c9da2fae9e59df18c8f86f0a9a4c77013ca100b
Buğrahan Y <78935299+Bqrayvzdgn@users.noreply.github.com>
2024-03-20 17:35:42 +0300

    Update macchange.py

--- macchange.py
+++ macchange.py
@@ -12,9 +12,9 @@ Logo = """
 """
 
 def get_user_input():
-    parser = argparse.ArgumentParser(description="This application was developed by kenxzz.", usage="[ Default ] python3 macchanger.py -i [interface] -m [XX:XX:XX:XX:XX:XX]", epilog="[ Helper ] -> python3 macchanger.py -h")
-    parser.add_argument("-i", "--iface", dest="interface", help="Interface to change!")
-    parser.add_argument("-m", "--mac", dest="mac_address", help="New mac address")
+    parser = argparse.ArgumentParser(Logo, description="This application was developed by kenxzz.", usage="python3 macchanger.py -i [interface] -m [XX:XX:XX:XX:XX:XX]", epilog="[ Helper ] -> python3 macchanger.py -h")
+    parser.add_argument("-i", "--iface", dest="interface", help="Enter your network interface")
+    parser.add_argument("-m", "--mac", dest="mac_address", help="Enter your mac address")
     return parser.parse_args()
 
 def change_mac_address(user_interface, user_mac_address):
