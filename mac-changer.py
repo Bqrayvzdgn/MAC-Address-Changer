40542f255fb3a569111c80d0b1a3ea74e282ae03
Buğrahan Y <78935299+Bqrayvzdgn@users.noreply.github.com>
2024-03-22 04:27:23 +0300

    Update mac-changer.py

--- mac-changer.py
+++ mac-changer.py
@@ -59,6 +59,5 @@ if __name__ == "__main__":
         print(f"Your MAC Address has been created : {str(user_input.mac_address)}")
     finally:
         print("Exiting.")
-        print(Banners.QUIT)
 else:
     print(Banners.ERROR)
