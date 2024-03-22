f84902513ec102b9d0d6c1a702ff05cb9f270052
Buğrahan Y <78935299+Bqrayvzdgn@users.noreply.github.com>
2024-03-22 04:24:11 +0300

    Update mac-changer.py

--- mac-changer.py
+++ mac-changer.py
@@ -51,8 +51,6 @@ if __name__ == "__main__":
     try:
         change_mac_address(user_input.interface, user_input.mac_address)
         print(f"Your MAC Address has been created : {str(user_input.mac_address)}")
-    except TypeError:
-        print(Banners.QUIT)
     finally:
         print("Exiting.")
         time.sleep(1)
